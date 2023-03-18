d4703fabef520437d2466ec044e530067e97c3ef
PedroPauloL <pedroloosx@gmail.com>
2023-03-18 12:48:51 -0300

    Implementar a camada de apresentação

---
+++ Api/Contact.cs
@@ -0,0 +1,7 @@
+namespace ContactsApi
+{
+    public class Contact
+    {
+        public string Name {get; set;}
+    }
+}
---
+++ Api/Controllers/ContactsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Api;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactsController : ControllerBase
+    {
+        [HttpPost]
+        public void Create([FromBody] Contact contact) 
+        {
+
+        }
+
+        [HttpGet]
+        public List<Contact> Get()
+        {
+            return new List<Contact>();
+        }  
+
+        [HttpPut("{id}")]
+        public void Update([FromBody] Contact updatedContact) 
+        {
+
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete([FromRoute] int id)
+        {
+
+        }
+    }
+}
+
